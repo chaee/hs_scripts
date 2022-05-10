8495bedaeb187d9249517ee26dfdd8225b1d3863
chaee <mcssfw@gmail.com>
2022-05-10 11:54:48 +0200

    add chung2019 conan

--- conan_modify.py
+++ conan_modify.py
@@ -1,3 +1,22 @@
-import
+import pandas as pd
 ds = '/Users/yunchaewon/gesis/datasets/CONAN/CONAN/CONAN.csv'
+df = pd.read_csv(ds) #, lineterminator='\n')
+source = 'chung2019'
+
+new_df = df[['hateSpeech', 'hsType', 'hsSubType']].copy()
+new_df = new_df.rename(columns={"hateSpeech": "text"})
+
+new_df.loc[:, "text"] = new_df["text"].apply(lambda x : x.replace('\n', '\\n'))
+new_df['label'] = df[df.columns[2:4]].apply(
+    lambda x: ','.join(x.dropna().astype(str)),
+    axis=1
+)
+
+new_df['source'] = source 
+new_df['language'] = df['cn_id'].apply(lambda x: x[:2])
+
+col_order = ['text', 'label', 'language', 'source'] #, 'dataset']
+new_df = new_df.reindex(columns=col_order)
+print(new_df.head())
+new_df.to_csv(f'formatted/{source}.csv', index=False, sep='\t')
 
---
+++ template.py
@@ -0,0 +1,20 @@
+import pandas as pd
+ds = '/Users/yunchaewon/gesis/datasets/CONAN/CONAN/CONAN.csv'
+df = pd.read_csv(ds, lineterminator='\n')
+new_df = df[['text', 'dataset']].copy()
+new_df.loc[:, "text"] = new_df["text"].apply(lambda x : x.replace('\n', '\\n'))
+
+#new_df['label'] = [(rdf.toxicity).strip(), str(df.sexist).strip()]
+new_df['label'] = df[df.columns[3:5]].apply(
+    lambda x: ','.join(x.dropna().astype(str)),
+    axis=1
+)
+
+new_df['source'] = 'samory2021en'
+new_df['language'] = 'en'
+col_order = ['text', 'label', 'language', 'source', 'dataset']
+new_df = new_df.reindex(columns=col_order)
+
+print(new_df.head())
+new_df.to_csv('formatted/samory2021en.csv', index=False, sep='\t')
+
